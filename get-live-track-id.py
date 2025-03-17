627f65b4fbae59f3bb9984f9dcff8a49bfbdb959
Michael Robinson <michael_robinson@live.ca>
2025-03-16 22:52:34 -0400

    dedicated script for track id

---
+++ get-live-track-id.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import curses, logging, socket, json
+
+import requests
+from prodj.core.prodj import ProDj
+import prodj.network.packets_dump as pdump
+
+# init logging
+default_loglevel=logging.INFO
+logging.basicConfig(level=default_loglevel)
+
+p = ProDj()
+p.set_client_keepalive_callback(lambda n: get_live_track_id())
+p.set_client_change_callback(lambda n: get_live_track_id())
+
+def get_live_track_id():
+  try:
+    decks,data = p.cl.clients,{"track_data": None}
+    decks_playing = sum(1 for d in decks if d.play_state == "playing")
+    print(decks_playing)
+    
+    if decks_playing == 1:
+      for d in decks:
+        print(d.play_state)
+        if d.play_state == "playing":
+          md = d.metadata
+          data["track_data"] = {
+            "artist": md["artist"],
+            "title": md["title"]
+          }
+    else:
+      data["live"] = "no decks playing or none found"
+    logging.info(data)
+    return json.dumps(data)
+  except:
+    logging.error("yeah something happened...")
+    raise
+
+# update_clients(client_win)
+
+try:
+  p.start()
+  p.vcdj_enable()
+  p.join()
+except KeyboardInterrupt:
+  logging.info("Shutting down...")
+  p.stop()
+
